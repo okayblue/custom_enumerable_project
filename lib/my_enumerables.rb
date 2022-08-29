d0d3197923741d1743d2f8152cab49f3177644ac
okayblue <56899411+okayblue@users.noreply.github.com>
2022-08-29 02:08:12 -0400

    Add my_each and my_all

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,16 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    # for el in self
+    #   return false if (yield el) == false
+    # end
+    my_each {|elem| return false if !yield elem}
+    true
+  end
+
+  def my_any
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +19,18 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for element in self do
+      yield element
+    end
+  end
 end
+
+
+# array = [1, 2, 3, 9]
+
+# array.my_each do |element|
+#   puts element
+# end
+
+# array.my_all? { |value| value < 5 }
