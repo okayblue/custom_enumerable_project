cf1b450dd9dc001e936c17e12ef0a354d8206b8b
okayblue <56899411+okayblue@users.noreply.github.com>
2022-08-30 01:48:44 -0400

    Create my_each_with_index, my_select, my_any?, and my_count

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,15 +1,39 @@
 module Enumerable
   # Your code goes here
   def my_all?
-    # for el in self
-    #   return false if (yield el) == false
-    # end
     my_each {|elem| return false if !yield elem}
     true
   end
 
-  def my_any
+  def my_each_with_index
+    index = 0
+    for elem in self do
+      yield elem, index
+      index+=1
+    end
   end
+
+  def my_select
+    new = []
+    my_each {|elem| new.push elem if yield elem}
+    new
+  end
+
+  def my_any?
+    my_each {|elem| return true if yield elem}
+    false
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      my_each {|elem| count += 1 if yield elem} 
+    else
+      count = self.length
+    end
+    count
+  end
+
 
 end
 
@@ -27,7 +51,18 @@ class Array
 end
 
 
-# array = [1, 2, 3, 9]
+# tests
+array = [1, 2, 3, 9, 33]
+
+# array.my_count { |value| value > 5 }
+
+# array.my_any?(&:negative?)
+
+# array.my_select { |value| value > 10 }
+
+# array.my_each_with_index do |element, index|
+#   puts "element: #{element}, index: #{index}"
+# end
 
 # array.my_each do |element|
 #   puts element
